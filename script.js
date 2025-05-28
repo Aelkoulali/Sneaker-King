018803e8403f1398cd0ac681ba7dcb02d7fae246
Azul <abdellah.elkoulali@gmx.fr>
2025-05-28 12:17:47 +0100

    Declare data

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 // Declare list of products
 const Data = [
     {
-
+        id : 1,
+        name : " ",
+        img : " ",
+        brand : " ",
+        amt : " ",
+        category: " ",
     },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-    {
-
-    },
-     {
-
-    },
-     {
-
-    },
-     {
-
-    },
+    
 ];
 
 // Declare variables
