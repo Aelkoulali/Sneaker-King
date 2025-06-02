f14fbace5f41942ae4b3669f9fd2d76905e9076a
Azul <abdellah.elkoulali@gmx.fr>
2025-06-02 12:39:12 +0100

    Update UA list

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ const Data = [
     {
         id : 17,
         name : "UA HAVOC 3",
-        img : ,
+        img :  { "src": "Public/Products/Under Rmour/UA HAVOC 3" },
         brand : "Under Armour",
         amt : "180",
         category: "Basketball",
@@ -139,7 +139,7 @@ const Data = [
     {
         id : 18,
         name : "UA Charged 10",
-        img : ,
+        img :  { "src": "Public/Products/Under Rmour/UA Charger 10" },
         brand : "Under Armour",
         amt : "90",
         category: "Running",
